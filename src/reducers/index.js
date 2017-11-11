e91fdad7f0f3d6bd17c1ac87cf7c1f138286b701
Stéphane Lam <stelam@gmail.com>
2017-11-10 22:08:43 -0500

    added redux-form, redirect to node war list after authentication

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import NavigationReducer from './reducer_navigation';
 import AuthenticationReducer from './reducer_authentication';
+import { reducer as formReducer} from 'redux-form'; 
 
 const rootReducer = combineReducers({
   navigationOpened: NavigationReducer,
-  loggedIn: AuthenticationReducer
+  loggedIn: AuthenticationReducer,
+  form: formReducer
 });
 
 export default rootReducer;
