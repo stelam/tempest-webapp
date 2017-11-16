c7f4b08f096b1347d6fbc88122ff42a2564ce1d6
Stéphane Lam <stelam@gmail.com>
2017-11-16 18:24:35 -0500

    updated webpack modules, added env variables

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,10 @@
+import axios from 'axios';
+import qs from 'qs';
+
 export const TOGGLE_NAVIGATION = 'TOGGLE_NAVIGATION';
 export const LOGIN = 'LOGIN';
+
+const API_BASE_URL = 'http://trusted-app:secret@localhost:9000/api/v1';
 
 export function toggleNavigation(navigationOpened) {
 
@@ -9,9 +14,27 @@ export function toggleNavigation(navigationOpened) {
   };
 }
 
-export function login(loggedIn) {
+export function login(values) {
+  var data = qs.stringify({ 
+              "grant_type": "password",
+              "username": "admin",
+              "password": "password"
+            });
+  const request = axios.request({
+    url: "/oauth/token",
+    baseURL: API_BASE_URL,
+    method: 'post',
+    auth: {
+      "username": process.env.TEMPEST_OAUTH_CLIENT_NAME,
+      "password": process.env.TEMPEST_OAUTH_CLIENT_SECRET
+    },
+    data: data,
+    withCredentials: true,
+    
+  }, data);
+
   return {
     type: LOGIN,
-    payload: loggedIn
+    payload: request
   }
 }
